1900a0c669d97acaa4c3e61cd6dbaaedc7885b12
sakicodes <officialkilan@gmail.com>
2026-03-17 11:11:24 +0800

    adding namespaces to program.cs

--- Data/UnitOfWork/AppDbContext.cs
+++ Data/UnitOfWork/AppDbContext.cs
@@ -177,7 +177,7 @@ public partial class AppDbContext : DbContext
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=pro_rental;Username=devuser;Password=devpassword");
+        => optionsBuilder.UseNpgsql("Host=localhost;Port=5432;Database=pro_rental;Username=REPLACE;Password=REPLACE");
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
