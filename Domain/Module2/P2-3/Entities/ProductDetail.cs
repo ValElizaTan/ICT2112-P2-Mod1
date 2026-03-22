6e4f95387cc0bba0aa4bda7a2d98a34654257029
yuchen310 <2402352@sit.singaporetech.edu.sg>
2026-03-22 14:38:22 +0800

    Implement Cost Calculation

--- Domain/Module2/P2-3/Entities/ProductDetail.cs
+++ Domain/Module2/P2-3/Entities/ProductDetail.cs
@@ -2,5 +2,6 @@ namespace ProRental.Domain.Entities;
 
 public partial class Productdetail
 {
-	
+    public decimal GetPrice() => _price;
+    public void SetPrice(decimal price) => _price = price;
 }
